e0f2521e1fe4875d1abaec92cf8f0b7d125223dc
Norman <normant_padovani@hotmail.com>
2023-03-21 15:14:14 -0400

    Task 6 use of init to import file storage method

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+import os
+from models.engine.db_storage import DBStorage # Import DBStorage class in this file
+from models.engine.file_storage import FileStorage # Import FileStorage class in this file
 
+if os.getenv('HBNB_TYPE_STORAGE') == "db":
+    storage = DBStorage() # Create an instance of DBStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation)
+    storage.reload()
 
-storage = FileStorage()
-storage.reload()
+else:
+    storage = FileStorage() # Create an instance of FileStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation)
+    storage.reload()
