759d07f811bbd0c7b97a6bd174cf1d1d18424ee8
Norman <normant_padovani@hotmail.com>
2023-03-21 12:48:25 -0400

    task 6 for city

--- models/city.py
+++ models/city.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel #Norman thingys, City inherits from BaseModel 
+from models.base_model import Base # Norman thingys, and Base (respect the order)
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities' # Norman thingys, Add or replace in the class City
+    name = Column(String(128), nullable=False) # Norman thingys, represents a column containing a string (128 characters), can’t be null
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False) # Norman thingys, represents a column containing a string (60 characters), can't be null, is a foreign key 
+    places = relationship("Place", backref='cities',
+                          cascade="all, delete, delete-orphan")
