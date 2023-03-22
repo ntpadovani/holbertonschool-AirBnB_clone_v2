71918a42ef01e5af4e70238d17e1733d8aab883d
Norman <normant_padovani@hotmail.com>
2023-03-22 10:10:58 -0400

    task 6 db choices

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 import os
-from models.engine.db_storage import DBStorage # Import DBStorage class in this file
-from models.engine.file_storage import FileStorage # Import FileStorage class in this file
 
 if os.getenv('HBNB_TYPE_STORAGE') == "db":
-    storage = DBStorage() # Create an instance of DBStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation)
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
     storage.reload()
 
 else:
-    storage = FileStorage() # Create an instance of FileStorage and store it in the variable storage (the line storage.reload() should be executed after this instantiation)
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
     storage.reload()
