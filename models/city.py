1ba0504cc129cecfc03b5a91a36709be3c624827
LizzGarleb <redscorpion3108@gmail.com>
2023-03-23 23:33:39 -0400

    city relationship

--- models/city.py
+++ models/city.py
@@ -3,14 +3,13 @@
 from models.base_model import BaseModel, Base #Norman thingys, City inherits from BaseModel 
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import environ
-
+import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
+    __tablename__ = 'cities' # Norman thingys, Add or replace in the class City
     name = Column(String(128), nullable=False) # Norman thingys, represents a column containing a string (128 characters), can’t be null
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False) # Norman thingys, represents a column containing a string (60 characters), can't be null, is a foreign key 
 
-    __tablename__ = 'cities'
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         places = relationship('Place', backref='cities', cascade='delete')
