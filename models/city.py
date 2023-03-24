70c5e7917a75dbf50956587cf36b6e1a5f4ff5cb
LizzGarleb <redscorpion3108@gmail.com>
2023-03-23 23:05:48 -0400

    moving L 11 to L14

--- models/city.py
+++ models/city.py
@@ -8,9 +8,9 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities' # Norman thingys, Add or replace in the class City
     name = Column(String(128), nullable=False) # Norman thingys, represents a column containing a string (128 characters), can’t be null
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False) # Norman thingys, represents a column containing a string (60 characters), can't be null, is a foreign key 
 
+    __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         places = relationship('Place', backref='cities', cascade='delete')
