23866cf6c62f60c97feede1576be54486b042829
Norman <normant_padovani@hotmail.com>
2023-03-24 21:49:22 -0400

    Pycodestyle fix

--- console.py
+++ console.py
@@ -334,8 +334,6 @@ class HBNBCommand(cmd.Cmd):
 
     def help_update(self):
         """ Help information for the update class """
-
-        
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
 
