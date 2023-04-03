13bfcc55790d96be55243c370a4ed0d4787127b3
ntpadovani <normant_padovani@hotmail.com>
2023-04-03 13:52:03 -0400

    Task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Displays the message"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hello2():
+    """Displays the message"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Displays 'C' with the variable text"""
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def py_text(text):
+    """Displays 'Python' with the variable text"""
+    return "Python %s" % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def int_text(n):
+    """Displays the variable 'n' only if it is an integer, in a message"""
+    return "%d is a number" % n
+
+if __name__ == "__main__":
+    app.run()
