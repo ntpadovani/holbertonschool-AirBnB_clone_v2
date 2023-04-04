620991ca8cfb0504427b61bf3e81595b5d5e5a24
ntpadovani <normant_padovani@hotmail.com>
2023-04-04 13:41:49 -0400

    task 8 sorting

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """ Teardown """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    dict_to_html = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                           Table="States",
+                           items=dict_to_html)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
